f6154f8f0e8d637cb2d932216c8d8606d25779dd
mo khan <mo.khan@gmail.com>
2020-02-04 15:54:00 -0700

    initial commit

---
+++ spec/spandx/rubygems_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Spandx::Rubygems do
+  it "has a version number" do
+    expect(Spandx::Rubygems::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
