d3087ae91da27c0c8b47f05fe62014f6d6044199
mo khan <mo.khan@gmail.com>
2020-02-04 19:31:44 -0700

    Add a thor CLI for building the rubygems.index

--- lib/spandx/rubygems.rb
+++ lib/spandx/rubygems.rb
@@ -1,5 +1,11 @@
-require "msgpack"
+require 'digest'
+require 'msgpack'
+require 'net/hippie'
+require 'nokogiri'
+require 'pg'
+require 'yaml'
 
+require "spandx/rubygems/index"
 require "spandx/rubygems/version"
 
 module Spandx
