8cca7946b70d9e49f7c039dd078b5aa1b2193370
mo khan <mo.khan@gmail.com>
2020-02-07 16:23:39 -0700

    Add subcommand to list out contents of the index

--- lib/spandx/rubygems/cli.rb
+++ lib/spandx/rubygems/cli.rb
@@ -4,9 +4,22 @@ require 'thor'
 module Spandx
   module Rubygems
     class CLI < Thor
+      desc 'list', 'List the rubygems.index'
+      def list
+        index.each do |id, license|
+          puts [id, license].inspect
+        end
+      end
+
       desc 'update', 'Updates the rubygems.index'
       def update
-        Spandx::Rubygems::Index.new.update!
+        index.update!
+      end
+
+      private
+
+      def index
+        Spandx::Rubygems::Index.new
       end
     end
   end
