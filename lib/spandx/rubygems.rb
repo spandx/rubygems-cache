5db2da4673df17ab1c6a08c19f04a6b93e27d251
mo khan <mo.khan@gmail.com>
2020-02-07 17:01:08 -0700

    compress index files

--- lib/spandx/rubygems.rb
+++ lib/spandx/rubygems.rb
@@ -1,9 +1,12 @@
+require 'bindata'
 require 'digest'
+require 'digest/crc'
 require 'msgpack'
 require 'net/hippie'
 require 'nokogiri'
 require 'pg'
 require 'yaml'
+require 'zlib'
 
 require "spandx/rubygems/index"
 require "spandx/rubygems/version"
