6018eb9bfa6fc5bdcdcb18393d480e0a121f09a2
mo khan <mo.khan@gmail.com>
2020-02-13 11:01:08 -0700

    Add API to retrieve licenses for a gem

---
+++ spec/spandx/index_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Spandx::Rubygems::Index do
+  describe "#licenses_for" do
+    [
+      { name: 'net-hippie', version: '0.3.2', licenses: ['MIT'] },
+    ].each do |item|
+      specify do
+        expect(
+          subject.licenses_for(name: item[:name], version: item[:version])
+        ).to match_array(item[:licenses])
+      end
+    end
+  end
+end
